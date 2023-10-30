3f8fca784addd1da4b4c8fd0a1cf917642550640
Michael Frieze <michaelfrieze@icloud.com>
2023-10-30 19:27:01 -0400

    Setting up some of the routes and pages

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+const AskQuestion = () => {
+  return (
+    <>
+      <div>Ask Question Page</div>
+    </>
+  );
+};
+
+export default AskQuestion;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold bg-slate-100">Main Page</h1>
-    </div>
-  );
-}
