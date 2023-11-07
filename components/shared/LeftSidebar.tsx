e1c91238739c1b727a441fa76dc1b102deb0b200
Michael Frieze <michaelfrieze@icloud.com>
2023-11-06 21:34:18 -0500

    Made leftSidebar a server component and moved nav items to their own client component

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,69 +1,17 @@
-"use client";
-
 import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
-import { SignedOut, useAuth } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
+import { SignedOut } from "@clerk/nextjs";
+import NavigationItem from "./NavigationItem";
 
 const LeftSidebar = () => {
-  const { userId } = useAuth();
-  const pathname = usePathname();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-  }
-
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
-        {sidebarLinks.map((item) => {
-          const isActive =
-            (pathname.includes(item.route) && item.route.length > 1) ||
-            pathname === item.route;
-
-          if (item.route === "/profile") {
-            if (userId) {
-              item.route = `${item.route}/${userId}`;
-            } else {
-              return null;
-            }
-          }
-
-          return (
-            <Link
-              href={item.route}
-              key={item.label}
-              className={`${
-                isActive
-                  ? "primary-gradient rounded-lg text-light-900"
-                  : "text-dark300_light900"
-              }  flex items-center justify-start gap-4 bg-transparent p-4`}
-            >
-              <Image
-                src={item.imgURL}
-                alt={item.label}
-                width={20}
-                height={20}
-                className={`${isActive ? "" : "invert-colors"}`}
-              />
-              <p
-                className={`${
-                  isActive ? "base-bold" : "base-medium"
-                } max-lg:hidden`}
-              >
-                {item.label}
-              </p>
-            </Link>
-          );
-        })}
+        {sidebarLinks.map((item) => (
+          <NavigationItem key={item.label} item={item} />
+        ))}
       </div>
 
       <SignedOut>
---
+++ components/shared/NavigationItem.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useAuth } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarLink } from "@/types";
+
+const NavigationItem = ({ item }: { item: SidebarLink }) => {
+  const { userId } = useAuth();
+  const pathname = usePathname();
+  // const [isMounted, setIsMounted] = useState(false);
+
+  // useEffect(() => {
+  //   setIsMounted(true);
+  // }, []);
+
+  // if (!isMounted) {
+  //   return null;
+  // }
+
+  const isActive =
+    (pathname.includes(item.route) && item.route.length > 1) ||
+    pathname === item.route;
+
+  if (item.route === "/profile") {
+    if (userId) {
+      item.route = `${item.route}/${userId}`;
+    } else {
+      return null;
+    }
+  }
+
+  return (
+    <>
+      <Link
+        href={item.route}
+        key={item.label}
+        className={`${
+          isActive
+            ? "primary-gradient rounded-lg text-light-900"
+            : "text-dark300_light900"
+        }  flex items-center justify-start gap-4 bg-transparent p-4`}
+      >
+        <Image
+          src={item.imgURL}
+          alt={item.label}
+          width={20}
+          height={20}
+          className={`${isActive ? "" : "invert-colors"}`}
+        />
+        <p
+          className={`${isActive ? "base-bold" : "base-medium"} max-lg:hidden`}
+        >
+          {item.label}
+        </p>
+      </Link>
+    </>
+  );
+};
+
+export default NavigationItem;
