b4cb045e42c31fc6b8f25683220bb671d105dbcc
Michael Frieze <michaelfrieze@icloud.com>
2023-11-07 20:30:20 -0500

    Fixed an error caused by react fragment in NavigationItem component

--- components/shared/NavigationItem.tsx
+++ components/shared/NavigationItem.tsx
@@ -9,15 +9,6 @@ import { SidebarLink } from "@/types";
 const NavigationItem = ({ item }: { item: SidebarLink }) => {
   const { userId } = useAuth();
   const pathname = usePathname();
-  // const [isMounted, setIsMounted] = useState(false);
-
-  // useEffect(() => {
-  //   setIsMounted(true);
-  // }, []);
-
-  // if (!isMounted) {
-  //   return null;
-  // }
 
   const isActive =
     (pathname.includes(item.route) && item.route.length > 1) ||
@@ -32,30 +23,26 @@ const NavigationItem = ({ item }: { item: SidebarLink }) => {
   }
 
   return (
-    <>
-      <Link
-        href={item.route}
-        key={item.label}
-        className={`${
-          isActive
-            ? "primary-gradient rounded-lg text-light-900"
-            : "text-dark300_light900"
-        }  flex items-center justify-start gap-4 bg-transparent p-4`}
-      >
-        <Image
-          src={item.imgURL}
-          alt={item.label}
-          width={20}
-          height={20}
-          className={`${isActive ? "" : "invert-colors"}`}
-        />
-        <p
-          className={`${isActive ? "base-bold" : "base-medium"} max-lg:hidden`}
-        >
-          {item.label}
-        </p>
-      </Link>
-    </>
+    <Link
+      href={item.route}
+      key={item.label}
+      className={`${
+        isActive
+          ? "primary-gradient rounded-lg text-light-900"
+          : "text-dark300_light900"
+      }  flex items-center justify-start gap-4 bg-transparent p-4`}
+    >
+      <Image
+        src={item.imgURL}
+        alt={item.label}
+        width={20}
+        height={20}
+        className={`${isActive ? "" : "invert-colors"}`}
+      />
+      <p className={`${isActive ? "base-bold" : "base-medium"} max-lg:hidden`}>
+        {item.label}
+      </p>
+    </Link>
   );
 };
 
